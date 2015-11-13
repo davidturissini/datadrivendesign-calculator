944dd3ab37bce3fd53f21f39e9e2efed511408f6
David Turissini <david@Davids-MacBook-Pro.local>
2015-11-12 20:04:48 -0700

    Exporting calculator

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 'use strict';
 
+const calculateSampleSize = require('./calculateSampleSize');
+const calculateSampleVariance = require('./calculateSampleVariance');
 const calculateSampleSizeFromBaselineConversionRate = require('./calculateSampleSizeFromBaselineConversionRate');
+
+module.exports = {
+    calculateSampleSize: calculateSampleSize,
+    calculateSampleVariance: calculateSampleVariance,
+    calculateSampleSizeFromBaselineConversionRate
+}
